41bf245bb25630a3608e057c18693809940a43d5
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2023-10-31 19:32:05 +0100

    add first userFunctions

---
+++ src/functions/userFunctions.php
@@ -0,0 +1,63 @@
+<?php
+
+function isEmailCorrect($connection,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."'");
+    return ($resultaat->num_rows == 0) ? false : true;
+}
+function isPasswordCorrect($connection,$password,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."'");
+    return (password_verify($password,$resultaat->fetch_assoc()['wachtwoord']))?true:false;
+}
+
+function registerUser($connection, $voornaam, $achternaam, $email, $wachtwoord, $profielfoto, $beschrijving) {
+    if(empty($profielfoto)) {
+        $profielfoto = "profile.png";
+    }
+
+    $wachtwoord = convertPasswordToHash($wachtwoord);
+
+    $resultaat = $connection->query("INSERT INTO tblgebruikers (email, voornaam, naam, wachtwoord, profielfoto, beschrijving) VALUES ('".$email."','".$voornaam."','".$achternaam."','".$wachtwoord."','".$profielfoto."','".$beschrijving."')");
+    return $resultaat;
+}
+
+function updateUser($connection, $gebruikerid, $voornaam, $achternaam, $email, $wachtwoord, $profielfoto, $beschrijving) {
+    if(empty($profielfoto)) {
+        if(getProfilePicture($connection,$gebruikerid)){
+            $profielfoto = getProfilePicture($connection,$gebruikerid);
+        }else{
+            print $connection->error;
+        }
+    }
+    if(empty($wachtwoord)){
+        $sql = "UPDATE tblgebruikers set email = '" . $email . "', voornaam = '" . $voornaam . "', naam = '" . $achternaam . "', profielfoto = '" . $profielfoto . "', beschrijving = '" . $beschrijving . "' where gebruikerid = '" . $gebruikerid . "'";
+        return ($connection->query($sql));
+    }else{
+        $wachtwoord = convertPasswordToHash($wachtwoord);
+
+        return ($connection->query("UPDATE tblgebruikers set email = '" . $email . "', voornaam = '" . $voornaam . "', naam = '" . $achternaam . "',
+        wachtwoord = '" . $wachtwoord . "', profielfoto = '" . $profielfoto . "', beschrijving = '" . $beschrijving . "' Where gebruikerid = '" . $gebruikerid . "'"));
+    }
+}
+
+function convertPasswordToHash($wachtwoord) {
+    $hashedwachtwoord = password_hash($wachtwoord, PASSWORD_DEFAULT);
+    return $hashedwachtwoord;
+}
+function getUser($connection,$gebruikerid){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where gebruikerid= '".$gebruikerid."'");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_all(MYSQLI_ASSOC);
+}
+function getProfilePicture($connection,$gebruikerid){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where gebruikerid= '".$gebruikerid."'");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_assoc()['profielfoto'];
+}
+
+function getGebruikersid($connection,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."'");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_assoc()['gebruikerid'];
+}
+function checkIfAdmin($connection,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."' and admin=1");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_all(MYSQLI_ASSOC);
+}
+?>
