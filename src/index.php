04890e579f0f4cf25a210a8f451ac7500724fde1
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2023-10-31 14:26:31 +0100

    Add basic Main page

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+include "navbar.php";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Game World</title>
+  <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.4/dist/full.css" rel="stylesheet" type="text/css" />
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-screen bg-[#F1FAEE]">
+  
+</body>
+</html>
