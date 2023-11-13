ee358ae2cc5a459fb167d6e0770e2e693cdd3f2e
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2023-11-13 18:04:23 +0100

    Reworked Space Invaders
    (first tutorial failed, so changed to another one)

--- src/Games/SpaceInvaders/SpaceInvaders.js
+++ src/Games/SpaceInvaders/SpaceInvaders.js
@@ -1,109 +1,208 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+let board;
+let boardWidth = tileSize * columns; // 32 * 16
+let boardHeight = tileSize * rows; // 32 * 16
+let context;
 
-class Player {
-    constructor() {
+//ship
+let shipWidth = tileSize*2;
+let shipHeight = tileSize;
+let shipX = tileSize * columns/2 - tileSize;
+let shipY = tileSize * rows - tileSize*2;
+
+let ship = {
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight
+}
+
+let shipImg;
+let shipVelocityX = tileSize; //ship moving speed
+
+//aliens
+let alienArray = [];
+let alienWidth = tileSize*2;
+let alienHeight = tileSize;
+let alienX = tileSize;
+let alienY = tileSize;
+let alienImg;
+
+let alienRows = 2;
+let alienColumns = 3;
+let alienCount = 0; //number of aliens to defeat
+let alienVelocityX = 1; //alien moving speed
+
+//bullets
+let bulletArray = [];
+let bulletVelocityY = -10; //bullet moving speed
+
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.width = boardWidth;
+    board.height = boardHeight;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    //draw initial ship
+    // context.fillStyle="green";
+    // context.fillRect(ship.x, ship.y, ship.width, ship.height);
 
 
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
-
-        const image = new Image()
-        image.src = './data/SpaceShip.png'
-        image.onload = () => {
-         this.image = image
-         this.width = image.width
-         this.heigth = image.heigth
-         this.position = {
-            x: (canvas.width) / 2 - (this.width / 2),
-            y: canvas.heigth - this.heigth - 20
-        } 
-        }
+    shipImg = new Image();
+    shipImg.src = "./data/SpaceShip.png";
+    shipImg.onload = function() {
+        context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     }
-        draw() {
-            c.drawImage(this.image, 
-                        this.position.x, 
-                        this.position.y, 
-                        this.width, 
-                        this.heigth)
+
+    alienImg = new Image();
+    alienImg.src = "./data/Alien.png";
+    createAliens();
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveShip);
+    document.addEventListener("keyup", shoot);
+}
+
+function update() {
+    requestAnimationFrame(update);
+
+    if (gameOver) {
+        return;
+    }
+
+    context.clearRect(0, 0, board.width, board.height);
+
+
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+
+
+    for (let i = 0; i < alienArray.length; i++) {
+        let alien = alienArray[i];
+        if (alien.alive) {
+            alien.x += alienVelocityX;
+
             
-            
-        }
-        update() {
-            if(this.image) {
-            this.draw()
-            this.position.x += this.velocity.x
+            if (alien.x + alien.width >= board.width || alien.x <= 0) {
+                alienVelocityX *= -1;
+                alien.x += alienVelocityX*2;
+
+               
+                for (let j = 0; j < alienArray.length; j++) {
+                    alienArray[j].y += alienHeight;
+                }
+            }
+            context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height);
+
+            if (alien.y >= ship.y) {
+                gameOver = true;
             }
         }
-        
     }
 
+    //schoten zelf
+    for (let i = 0; i < bulletArray.length; i++) {
+        let bullet = bulletArray[i];
+        bullet.y += bulletVelocityY;
+        context.fillStyle="white";
+        context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
 
-const player = new Player()
-const keys = {
-    q: {
-        pressed:false
-    },
-    d: {
-        pressed:false
-    },
-    space: {
-        pressed:false
-    },
+        //schoten raken alien
+        for (let j = 0; j < alienArray.length; j++) {
+            let alien = alienArray[j];
+            if (!bullet.used && alien.alive && detectCollision(bullet, alien)) {
+                bullet.used = true;
+                alien.alive = false;
+                alienCount--;
+                score += 100;
+            }
+        }
+    }
 
+    //schoten verwijderen
+    while (bulletArray.length > 0 && (bulletArray[0].used || bulletArray[0].y < 0)) {
+        bulletArray.shift();
+    }
+
+    //volgende level
+    if (alienCount == 0) {
+        //aliens bijvoegen nadat alle aliens gedood zijn
+        score += alienColumns * alienRows * 100; //bonus punten als de level clear is :)
+        alienColumns = Math.min(alienColumns + 1, columns/2 -2);
+        alienRows = Math.min(alienRows + 1, rows-4);
+        if (alienVelocityX > 0) {
+            alienVelocityX += 0.1;
+        }
+        else {
+            alienVelocityX -= 0.1; 
+        }
+        alienArray = [];
+        bulletArray = [];
+        createAliens();
+    }
+
+    //score
+    context.fillStyle="white";
+    context.font="16px courier";
+    context.fillText(score, 5, 20);
 }
 
-function animate() {
-    requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)   
-    player.update()
+function moveShip(e) {
+    if (gameOver) {
+        return;
+    }
 
-    if(keys.q.pressed && player.position.x >= 0) {
-        player.velocity.x = -5
-    } else if (keys.d.pressed && player.position.x +player.width <= canvas.width) {
-        player.velocity.x = 5
-
-    } else {
-        player.velocity.x = 0
+    if (e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0) {
+        ship.x -= shipVelocityX; 
+    }
+    else if (e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width) {
+        ship.x += shipVelocityX; 
     }
 }
 
-animate()
-
-addEventListener('keydown', ({ key }) => {
-    switch(key) {
-        case 'q':
-            console.log('left')
-            keys.q.pressed = true
-            break
-        case 'd':
-            console.log('right')
-            keys.d.pressed = true
-            break
-        case ' ':
-            console.log('space')
-            break   
+function createAliens() {
+    for (let c = 0; c < alienColumns; c++) {
+        for (let r = 0; r < alienRows; r++) {
+            let alien = {
+                img : alienImg,
+                x : alienX + c*alienWidth,
+                y : alienY + r*alienHeight,
+                width : alienWidth,
+                height : alienHeight,
+                alive : true
+            }
+            alienArray.push(alien);
+        }
     }
-}) 
+    alienCount = alienArray.length;
+}
 
-addEventListener('keyup', ({ key }) => {
-    switch(key) {
-        case 'q':
-            console.log('left')
-            keys.q.pressed = false
-            break
-        case 'd':
-            console.log('right')
-            keys.d.pressed = false
-            break
-        case ' ':
-            console.log('space')
-            break   
+function shoot(e) {
+    if (gameOver) {
+        return;
     }
-}) 
+
+    if (e.code == "Space") {
+
+        let bullet = {
+            x : ship.x + shipWidth*15/32,
+            y : ship.y,
+            width : tileSize/8,
+            height : tileSize/2,
+            used : false
+        }
+        bulletArray.push(bullet);
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&   
+           a.x + a.width > b.x &&   
+           a.y < b.y + b.height &&  
+           a.y + a.height > b.y;    
+}
--- src/Games/SpaceInvaders/SpaceInvaders.php
+++ src/Games/SpaceInvaders/SpaceInvaders.php
@@ -1,11 +1,15 @@
-<canvas></canvas>
-<head>
-  <meta charset="UTF-8" />
-  <title>Space Invaders</title>
-  <script src="SpaceInvaders.js"></script>
-</head>
-<style>
-    body {
-        margin: 0;
-    }
-</style>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport", content="width=device-width, initial-scale=1.0">
+        <title>Space Invaders</title>
+        <link rel="stylesheet" href="SpaceInvaders.css">
+        <script src="SpaceInvaders.js"></script>
+    </head>
+
+    <body>
+        <h1>Space Invaders</h1>
+        <canvas id="board"></canvas>
+    </body>
+</html>
