2c93628904232f4a908a02d5c92f9ec72abd577a
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2023-11-24 16:12:01 +0100

    Change shooting back to manual

--- src/Games/SpaceInvaders/SpaceInvaders.js
+++ src/Games/SpaceInvaders/SpaceInvaders.js
@@ -63,7 +63,7 @@ window.onload = function() {
 
     requestAnimationFrame(update);
     document.addEventListener("keydown", moveShip);
-    document.addEventListener("keydown", shoot);
+    document.addEventListener("keyup", shoot);
 }
 
 function update() {
