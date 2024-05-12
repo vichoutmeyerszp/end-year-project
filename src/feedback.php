ced528af6d4cbbb378d742b18e404a9c6a3ecb64
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2024-05-12 15:42:03 +0200

    fix feedback.php

--- src/feedback.php
+++ src/feedback.php
@@ -1,9 +1,6 @@
 <?php
 include "components/navbar.php";
 
- if(isset($_POST['Feedback'])) {
-    addReview($mysqli, $_POST['spel'], $_SESSION['login'], $_POST['feedback'], $_POST['rating-10']);
-    }
 ?>
 
 
@@ -21,6 +18,17 @@ include "components/navbar.php";
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
+<?php
+ if(isset($_POST['Feedback'])) {
+  addReview($mysqli, $_POST['spel'], $_SESSION['login'], $_POST['feedback'], $_POST['rating-10']);
+  echo '<div role="alert" class="alert alert-success">
+  <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z mx-auto" /></svg>
+  <span>Uw feedback is met succes doorgestuurd!</span>
+</div>';
+  }
+
+
+?>
   <h1 class="md:text-center text-4xl font-bold mb-8">Geef uw feedback</h1>
 
   <form action="feedback.php" method="post" enctype="multipart/form-data" class="flex flex-col gap-8 w-full md:max-w-2xl  mx-auto">
@@ -63,14 +71,14 @@ include "components/navbar.php";
             <span class="label-text md:text-center">Op een score van 10, hoeveel geef je het?</span>
           </label>
           <div class="rating rating-lg rating-half">
-            <input type="radio" name="rating-10" class="rating-hidden" />
+            <input type="radio" name="rating-10" value="0"  class="rating-hidden" />
             <input type="radio" name="rating-10" value="1"  class="bg-yellow-400 mask mask-star-2 mask-half-1" />
             <input type="radio" name="rating-10" value="2"  class="bg-yellow-400 mask mask-star-2 mask-half-2" />
             <input type="radio" name="rating-10" value="3"  class="bg-yellow-400 mask mask-star-2 mask-half-1" />
             <input type="radio" name="rating-10" value="4"  class="bg-yellow-400 mask mask-star-2 mask-half-2" />
             <input type="radio" name="rating-10" value="5"  class="bg-yellow-400 mask mask-star-2 mask-half-1" />
             <input type="radio" name="rating-10" value="6"  class="bg-yellow-400 mask mask-star-2 mask-half-2" />
-            <input type="radio" name="rating-10" value="8"  class="bg-yellow-400 mask mask-star-2 mask-half-1" />
+            <input type="radio" name="rating-10" value="7"  class="bg-yellow-400 mask mask-star-2 mask-half-1" />
             <input type="radio" name="rating-10" value="8"  class="bg-yellow-400 mask mask-star-2 mask-half-2" />
             <input type="radio" name="rating-10" value="9"  class="bg-yellow-400 mask mask-star-2 mask-half-1" />
             <input type="radio" name="rating-10" value="10" class="bg-yellow-400 mask mask-star-2 mask-half-2" />
@@ -78,3 +86,7 @@ include "components/navbar.php";
         </div>
     <button name="Feedback" class="btn btn-primary">Geef feedback</button>
   </form>
+
+  <?php
+      
+      ?>
